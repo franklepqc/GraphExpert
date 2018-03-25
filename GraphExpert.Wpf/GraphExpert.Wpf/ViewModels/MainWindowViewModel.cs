9e59eb4cab9dea15ac13ca180d4b32f4a5c32ccc
François Lépine <lepine.francois@gatineau.ca>
2018-03-24 20:55:47 -0400

    Petits ajouts.

--- GraphExpert.Wpf/GraphExpert.Wpf/ViewModels/MainWindowViewModel.cs
+++ GraphExpert.Wpf/GraphExpert.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace GraphExpert.Wpf.ViewModels
@@ -7,11 +8,16 @@ namespace GraphExpert.Wpf.ViewModels
     {
         public ObservableCollection<StopVM> Formes { get; private set; } = new ObservableCollection<StopVM>();
 
-        public void AjouterArret(double x, double y)
+        public void AjouterArret(double x, double y, string etiquette)
         {
             var arret = new StopVM(x, y, (true == Formes?.Any() ? (byte)(Formes.Max(k => k.Id) + 1) : (byte)1) , "");
 
             Formes.Add(arret);
+        }
+
+        public bool AjouterLiaison(StopVM arret1, StopVM arret2)
+        {
+            return true;
         }
     }
 }
--- GraphExpert.Wpf/GraphExpert.Wpf/Views/MainWindow.xaml.cs
+++ GraphExpert.Wpf/GraphExpert.Wpf/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using GraphExpert.Wpf.ViewModels;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GraphExpert.Wpf.Views
 {
@@ -8,6 +9,8 @@ namespace GraphExpert.Wpf.Views
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
+        private StopVM _arret1;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,9 +20,28 @@ namespace GraphExpert.Wpf.Views
 
         private void Canvas_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var position = e.GetPosition(e.Source as IInputElement);
+            if (e.OriginalSource.GetType() == typeof(Canvas))
+            {
+                var position = e.GetPosition(e.Source as IInputElement);
 
-            Modele.AjouterArret(position.X - 15, position.Y - 15);
+                Modele.AjouterArret(position.X - 15, position.Y - 15, string.Empty);
+            }
+            else if (_arret1 == null)
+            {
+                _arret1 = ObtenirStop(e.OriginalSource);
+            }
+            else
+            {
+                var arret2 = ObtenirStop(e.OriginalSource);
+
+                // Faire la liaison.
+                if (Modele.AjouterLiaison(_arret1, arret2)) _arret1 = null;
+            }
+        }
+
+        private StopVM ObtenirStop(object element)
+        {
+            return (element as FrameworkElement).DataContext as StopVM;
         }
     }
 }
